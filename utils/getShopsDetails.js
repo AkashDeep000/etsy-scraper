cb4a2a21a929b212fcad6d902c13d7b6fe61b234
NAME <mail@gmail.com>
2023-08-05 07:27:21 +0530

    added axios header

--- utils/getShopsDetails.js
+++ utils/getShopsDetails.js
@@ -21,7 +21,25 @@ const getShopsDetailsAll = async (shopsLink, batchSize, keyword) => {
     //Get shop details
     const shopDetails = {};
 
-    const res = await axios.get(shopLink);
+    const res = await axios.get(shopLink,     {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,bn;q=0.6",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua": '"Not:A-Brand";v="99", "Chromium";v="112"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 11; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
+      },
+    });
     const html = await res.data;
     //console.log(html);
     const $ = cheerio.load(html);
@@ -80,7 +98,26 @@ const getShopsDetailsAll = async (shopsLink, batchSize, keyword) => {
       const res = await axios.get(
         `${shopLink.split("?")[0]}/reviews?page=${Math.ceil(
           shopDetails.return / 14
-        )}`
+        )}`,
+            {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,bn;q=0.6",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua": '"Not:A-Brand";v="99", "Chromium";v="112"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 11; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
+      },
+    }
       );
 
       const html = await res.data;
--- utils/getShopsUrl.js
+++ utils/getShopsUrl.js
@@ -19,7 +19,26 @@ const getShopsUrl = async (productsUrl, batchSize, keyword) => {
   b1.start(productsUrl.length, 0);
 
   const getShopUrl = async (productUrl) => {
-    const res = await axios.get(productUrl);
+    const res = await axios.get(productUrl,     {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,bn;q=0.6",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua": '"Not:A-Brand";v="99", "Chromium";v="112"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Linux; Android 11; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36",
+      },
+    }
+    );
     const html = await res.data;
     //console.log(html);
     const $ = cheerio.load(html);
